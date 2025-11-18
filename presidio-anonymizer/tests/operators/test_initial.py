5121ba31fa09871ea3ba2fab7c934455b8067a30
Miles Padot <155395178+mpadot@users.noreply.github.com@users.noreply.github.com>
2025-11-18 19:25:34 +0000

    Finished task 3

---
+++ presidio-anonymizer/tests/operators/test_initial.py
@@ -0,0 +1,13 @@
+from presidio_anonymizer.operators.initial import Initial
+def test_correct_name():
+    assert Initial().operator_name() == "initial"
+
+    @pytest.mark.parametrize(
+    "input_text, initials",
+    [
+        ("John Smith", "J. S."),
+    ],
+)
+def test_given_value_for_initial(input_text, initials):
+    text = Initial().operate(input_text) == initials
+    assert text == initials
