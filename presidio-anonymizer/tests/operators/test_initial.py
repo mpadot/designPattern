c5162fab77d1efa7ec08aba98f97b49c715b9f3d
Miles Padot <155395178+mpadot@users.noreply.github.com@users.noreply.github.com>
2025-11-18 20:08:09 +0000

    Finished tast 7 and 8

--- presidio-anonymizer/tests/operators/test_initial.py
+++ presidio-anonymizer/tests/operators/test_initial.py
@@ -1,18 +1,21 @@
 import pytest
 from presidio_anonymizer.operators.initial import Initial
+
+
 def test_correct_name():
     assert Initial().operator_name() == "initial"
 
-    @pytest.mark.parametrize(
+
+@pytest.mark.parametrize(  
     "input_text, initials",
     [
         ("John Smith", "J. S."),
         ("     Eastern    Michigan   University ", "E. M. U."),
-        
-
-
+        ("@abc", "@A."),
+        ("@843A", "@8."),
+        ("--**abc", "--**A."),
     ],
 )
-    def test_given_value_for_initial(input_text, initials):
-        text = Initial().operate(input_text) == initials
-        assert text == initials
+def test_given_value_for_initial(input_text, initials):  
+    result = Initial().operate(input_text)  
+    assert result == initials
